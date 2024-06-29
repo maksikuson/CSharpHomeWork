b9aff0423c3b43e2577d35b79f571623e3ac3bc7
Your Name <you@example.com>
2024-06-29 23:59:20 +0300

    Start

---
+++ 05_ClassStructRef/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05_ClassStructRef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
